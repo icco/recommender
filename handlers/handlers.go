4f5567b6a6d3539e9028bd4d121dbdb787be63ea
Nat Welch <nat@natwelch.com>
2025-04-02 16:56:50 +0000

    Remove duplicate statsData type from handlers package

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -20,20 +20,6 @@ type errorData struct {
 	Message string
 }
 
-type statsData struct {
-	TotalRecommendations        int64
-	TotalMovies                 int64
-	TotalAnime                  int64
-	TotalTVShows                int64
-	FirstDate                   time.Time
-	LastDate                    time.Time
-	AverageDailyRecommendations float64
-	GenreDistribution           []struct {
-		Genre string
-		Count int64
-	}
-}
-
 // renderError renders an error page using the error template.
 // It takes a response writer, error message, and HTTP status code.
 func renderError(w http.ResponseWriter, message string, status int) {
