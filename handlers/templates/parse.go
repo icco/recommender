4b5a357c2be4838e0959f49660918fa7c217f9cf
Nat Welch <nat@natwelch.com>
2025-04-02 18:27:14 +0000

    Add template functions for pagination calculations

--- handlers/templates/parse.go
+++ handlers/templates/parse.go
@@ -6,5 +6,14 @@ import "html/template"
 // It takes a variadic list of template file paths and returns a parsed template
 // or an error if parsing fails.
 func ParseTemplates(files ...string) (*template.Template, error) {
-	return template.ParseFS(FS, files...)
+	funcMap := template.FuncMap{
+		"add": func(a, b int) int {
+			return a + b
+		},
+		"subtract": func(a, b int) int {
+			return a - b
+		},
+	}
+
+	return template.New("").Funcs(funcMap).ParseFS(FS, files...)
 }
